a3a594b2577d70c09af1447869425cb291025109
Epangelias <epangelias@gmail.com>
2024-10-05 16:13:43 -0500

    update for web app tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,10 +5,18 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Verbum</title>
         <link rel="stylesheet" href="/css/main.css" />
         <meta name="color-scheme" content="light dark" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* <meta name='theme-color' content='#39a3c5' /> */}
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta
+          content="minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no"
+          name="viewport"
+        />
       </head>
       <body>
         <main>
